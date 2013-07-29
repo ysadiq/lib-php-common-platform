d17cedbdb3a17c2ae838892a65832581f9c44655
Jerry Ablan <jerryablan@dreamfactory.com>
2013-07-29 13:32:14 -0400

    portal tweaks, added facebook client

--- Yii/Models/App.php
+++ Yii/Models/App.php
@@ -43,7 +43,7 @@ use Kisma\Core\Utility\Sql;
  * @property string               $import_url
  * @property string               $storage_service_id
  * @property string               $storage_container
- * @property string               $_launchUrl
+ * @property string               $launch_url
  * @property boolean              $requires_fullscreen
  * @property boolean              $allow_fullscreen_toggle
  * @property boolean              $toggle_location
@@ -68,7 +68,7 @@ class App extends BasePlatformSystemModel
 	/**
 	 * @var string The url to launch the app from, comprised of its storage and starting file.
 	 */
-	protected $_launchUrl;
+	public $launch_url;
 
 	//*************************************************************************
 	//* Methods
@@ -95,7 +95,7 @@ class App extends BasePlatformSystemModel
 				array( 'name, api_name', 'length', 'max' => 64 ),
 				array( 'storage_container', 'length', 'max' => 255 ),
 				array(
-					'name, api_name, description, is_active, url, is_url_external, import_url, storage_service_id, storage_container, _launchUrl, requires_fullscreen, allow_fullscreen_toggle, toggle_location, requires_plugin',
+					'name, api_name, description, is_active, url, is_url_external, import_url, storage_service_id, storage_container, launch_url, requires_fullscreen, allow_fullscreen_toggle, toggle_location, requires_plugin',
 					'safe'
 				),
 			);
@@ -244,7 +244,7 @@ class App extends BasePlatformSystemModel
 		if ( !$this->is_url_external )
 		{
 			$_protocol = ( isset( $_SERVER['HTTPS'] ) && $_SERVER['HTTPS'] != 'off' ) ? 'https' : 'http';
-			$this->_launchUrl = $_protocol . '://' . FilterInput::server( 'HTTP_HOST' ) . '/';
+			$this->launch_url = $_protocol . '://' . FilterInput::server( 'HTTP_HOST' ) . '/';
 
 			if ( !empty( $this->storage_service_id ) )
 			{
@@ -253,24 +253,24 @@ class App extends BasePlatformSystemModel
 
 				if ( !empty( $_service ) )
 				{
-					$this->_launchUrl .= $_service->api_name . '/';
+					$this->launch_url .= $_service->api_name . '/';
 				}
 
 				if ( !empty( $this->storage_container ) )
 				{
-					$this->_launchUrl .= $this->storage_container . '/';
+					$this->launch_url .= $this->storage_container . '/';
 				}
 			}
 			else
 			{
-				$this->_launchUrl .= 'app/applications/';
+				$this->launch_url .= 'app/applications/';
 			}
 
-			$this->_launchUrl .= $this->api_name . $this->url;
+			$this->launch_url .= $this->api_name . $this->url;
 		}
 		else
 		{
-			$this->_launchUrl = $this->url;
+			$this->launch_url = $this->url;
 		}
 	}
 
@@ -296,7 +296,7 @@ class App extends BasePlatformSystemModel
 					 'import_url',
 					 'storage_service_id',
 					 'storage_container',
-					 '_launchUrl',
+					 'launch_url',
 					 'requires_fullscreen',
 					 'allow_fullscreen_toggle',
 					 'toggle_location',
