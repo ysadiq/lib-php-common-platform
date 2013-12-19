f331961354059c3c17e2481ab7a42dc7478aa4d4
Jerry Ablan <jerryablan@gmail.com>
2013-12-19 00:39:52 -0500

    Portal update for using "provider_name" as template

--- Services/BasePlatformRestService.php
+++ Services/BasePlatformRestService.php
@@ -178,8 +178,8 @@ abstract class BasePlatformRestService extends BasePlatformService implements Re
 		if ( false === ( $this->_response = $this->_handleResource() ) )
 		{
 			$_message = $this->_action . ' requests' .
-						( !empty( $this->_resource ) ? ' for resource "' . $this->_resourcePath . '"' : ' without a resource' ) .
-						' are not currently supported by the "' . $this->_apiName . '" service.';
+				( !empty( $this->_resource ) ? ' for resource "' . $this->_resourcePath . '"' : ' without a resource' ) .
+				' are not currently supported by the "' . $this->_apiName . '" service.';
 
 			throw new BadRequestException( $_message );
 		}
@@ -286,7 +286,7 @@ abstract class BasePlatformRestService extends BasePlatformService implements Re
 
 		if ( $this->_autoDispatch && method_exists( $this, $_method ) )
 		{
-			return call_user_func( array( $this, $_method ) );
+			return call_user_func( array($this, $_method) );
 		}
 
 		//	Otherwise just return false
@@ -396,7 +396,7 @@ abstract class BasePlatformRestService extends BasePlatformService implements Re
 			else
 			{
 				RestResponse::sendErrors(
-					new BadRequestException( 'No application name header or parameter value in request.' )
+							new BadRequestException( 'No application name header or parameter value in request.' )
 				);
 			}
 		}
