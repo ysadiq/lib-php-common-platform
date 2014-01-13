a043652b3650756567721f7358bd0954d8d16ab2
Jerry Ablan <jerryablan@dreamfactory.com>
2014-01-12 20:34:17 -0500

    Add new standard path getters to Platform utility. Also added supporting enum LocalStorageTypes.

--- Utility/Platform.php
+++ Utility/Platform.php
@@ -19,8 +19,10 @@
  */
 namespace DreamFactory\Platform\Utility;
 
+use DreamFactory\Platform\Enums\LocalStorageTypes;
 use DreamFactory\Yii\Utility\Pii;
 use Kisma\Core\SeedUtility;
+use Kisma\Core\Utility\Option;
 
 /**
  * Platform
@@ -33,6 +35,39 @@ class Platform extends SeedUtility
 	//*************************************************************************
 
 	/**
+	 * Constructs a virtual platform path
+	 *
+	 * @param string $type The type of path, used as a key into config
+	 * @param string $append
+	 *
+	 * @throws InvalidArgumentException
+	 * @return string
+	 */
+	protected static function _getPlatformPath( $type, $append = null )
+	{
+		static $_cache = array();
+
+		if ( !LocalStorageTypes::contains( $type ) )
+		{
+			throw new \InvalidArgumentException( 'Type "' . $type . '" is invalid.' );
+		}
+
+		if ( null === ( $_path = Option::get( $_cache, $type ) ) )
+		{
+			$_base = Pii::getParam( $type . '_path' );
+
+			if ( !file_exists( $_base ) )
+			{
+				@\mkdir( $_base, 0777, true );
+			}
+
+			$_path = $_base . ( $append ? '/' . $append : null );
+		}
+
+		return $_path;
+	}
+
+	/**
 	 * Constructs the virtual storage path
 	 *
 	 * @param string $append
@@ -40,6 +75,32 @@ class Platform extends SeedUtility
 	 * @return string
 	 */
 	public static function getStoragePath( $append = null )
+	{
+		static $_path = null;
+
+		if ( null === $_path )
+		{
+			$_base = Pii::getParam( 'storage_path' );
+
+			if ( !file_exists( $_base ) )
+			{
+				@\mkdir( $_base, 0777, true );
+			}
+
+			$_path = $_base . ( $append ? '/' . $append : null );
+		}
+
+		return $_path;
+	}
+
+	/**
+	 * Constructs the virtual private path
+	 *
+	 * @param string $append
+	 *
+	 * @return string
+	 */
+	public static function getPrivatePath( $append = null )
 	{
 		$_base = Pii::getParam( 'storage_path' );
 
@@ -64,27 +125,13 @@ class Platform extends SeedUtility
 			hash(
 				'ripemd128',
 				uniqid( '', true ) . ( $_uuid ? : microtime( true ) ) . md5(
-					$namespace . $_SERVER['REQUEST_TIME']
-					. $_SERVER['HTTP_USER_AGENT']
-					. $_SERVER['LOCAL_ADDR']
-					. $_SERVER['LOCAL_PORT']
-					. $_SERVER['REMOTE_ADDR']
-					. $_SERVER['REMOTE_PORT']
+					$namespace . $_SERVER['REQUEST_TIME'] . $_SERVER['HTTP_USER_AGENT'] . $_SERVER['LOCAL_ADDR'] . $_SERVER['LOCAL_PORT'] . $_SERVER['REMOTE_ADDR'] .
+					$_SERVER['REMOTE_PORT']
 				)
 			)
 		);
 
-		$_uuid = '{' .
-				 substr( $_hash, 0, 8 ) .
-				 '-' .
-				 substr( $_hash, 8, 4 ) .
-				 '-' .
-				 substr( $_hash, 12, 4 ) .
-				 '-' .
-				 substr( $_hash, 16, 4 ) .
-				 '-' .
-				 substr( $_hash, 20, 12 ) .
-				 '}';
+		$_uuid = '{' . substr( $_hash, 0, 8 ) . '-' . substr( $_hash, 8, 4 ) . '-' . substr( $_hash, 12, 4 ) . '-' . substr( $_hash, 16, 4 ) . '-' . substr( $_hash, 20, 12 ) . '}';
 
 		return $_uuid;
 	}
