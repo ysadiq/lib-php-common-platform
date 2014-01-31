0cd9cf14190f5e4fb2fb9091c6d1af89693a0d53
Jerry Ablan <jerryablan@dreamfactory.com>
2014-01-31 12:30:23 -0500

    Some more changes phpunit.xml for version 3.7.x

---
+++ tests/config/constants.config.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * This file is part of the DreamFactory Services Platform(tm) (DSP)
+ *
+ * DreamFactory Services Platform(tm) <http://github.com/dreamfactorysoftware/dsp-core>
+ * Copyright 2012-2013 DreamFactory Software, Inc. <developer-support@dreamfactory.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+//*************************************************************************
+//* Constants
+//*************************************************************************
+
+/**
+ * @var string
+ */
+const DSP_VERSION = '1.4.x-dev';
+/**
+ * @var string
+ */
+const API_VERSION = '1.0';
+/**
+ * @var string
+ */
+const ALIASES_CONFIG_PATH = '/aliases.config.php';
+/**
+ * @var string
+ */
+const SERVICES_CONFIG_PATH = '/services.config.php';
+/**
+ * @var string
+ */
+const DEFAULT_CLOUD_API_ENDPOINT = 'http://api.cloud.dreamfactory.com';
+/**
+ * @var string
+ */
+const DEFAULT_INSTANCE_AUTH_ENDPOINT = 'http://cerberus.fabric.dreamfactory.com/api/instance/credentials';
+/**
+ * @var string
+ */
+const DEFAULT_SUPPORT_EMAIL = 'support@dreamfactory.com';
+/**
+ * @var string
+ */
+const DEFAULT_ADMIN_RESOURCE_SCHEMA = '/admin.resource_schema.config.php';
