b3538415899485f420cc9a6b71897324e6eb7b91
Jerry Ablan <jerryablan@dreamfactory.com>
2014-02-10 10:36:24 -0500

    Added use statement for Curl

--- Services/SystemManager.php
+++ Services/SystemManager.php
@@ -42,6 +42,7 @@ use DreamFactory\Platform\Yii\Models\EmailTemplate;
 use DreamFactory\Platform\Yii\Models\Service;
 use DreamFactory\Platform\Yii\Models\User;
 use DreamFactory\Yii\Utility\Pii;
+use Kisma\Core\Interfaces\HttpResponse;
 use Kisma\Core\Utility\Curl;
 use Kisma\Core\Utility\Log;
 use Kisma\Core\Utility\Option;
