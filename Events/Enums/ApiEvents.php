e12b5c697a8211cd3c6117747aa25db7a85c999f
Jerry Ablan <jerryablan@dreamfactory.com>
2014-02-11 23:50:50 -0500

    EventManager added. BaseApiController added.

---
+++ Events/Enums/ApiEvents.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * This file is part of the DreamFactory Services Platform(tm) (DSP)
+ *
+ * DreamFactory Services Platform(tm) <http://github.com/dreamfactorysoftware/dsp-core>
+ * Copyright 2012-2013 DreamFactory Software, Inc. <support@dreamfactory.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+namespace DreamFactory\Platform\Events\Enums;
+
+/**
+ * The base events raised by API controllers
+ */
+class ApiEvents extends ObjectEvents
+{
+	/**
+	 * @var string Called before the request is dispatched
+	 */
+	const BEFORE_REQUEST = '{request_uri}.{method}.before_request';
+	/**
+	 * @var string Called after the dispatch request returns
+	 */
+	const AFTER_REQUEST = '{request_uri}.{method}.after_request';
+}
