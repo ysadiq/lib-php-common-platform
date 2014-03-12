f986c49e8da00d799e1f2be913e89cccabe39d8a
Jerry Ablan <jerryablan@dreamfactory.com>
2014-03-12 09:44:24 -0400

    Added swagger file for script runner service

--- src/Enums/PlatformServiceTypes.php
+++ src/Enums/PlatformServiceTypes.php
@@ -27,126 +27,127 @@ use Kisma\Core\Utility\Option;
  */
 class PlatformServiceTypes extends SeedEnum
 {
-	//*************************************************************************
-	//* Constants
-	//*************************************************************************
+    //*************************************************************************
+    //* Constants
+    //*************************************************************************
 
-	/**
-	 * @var int
-	 */
-	const SYSTEM_SERVICE = 0x0000;
-	/**
-	 * @var int
-	 */
-	const EMAIL_SERVICE = 0x0001;
-	/**
-	 * @var int
-	 */
-	const LOCAL_FILE_STORAGE = 0x0002;
-	/**
-	 * @var int
-	 */
-	const LOCAL_SQL_DB = 0x0004;
-	/**
-	 * @var int
-	 */
-	const LOCAL_SQL_DB_SCHEMA = 0x0008;
-	/**
-	 * @var int
-	 */
-	const NOSQL_DB = 0x0010;
-	/**
-	 * @var int
-	 */
-	const SALESFORCE = 0x0020;
-	/**
-	 * @var int
-	 */
-	const LOCAL_PORTAL_SERVICE = 0x0040;
-	/**
-	 * @var int
-	 */
-	const REMOTE_FILE_STORAGE = 0x1002;
-	/**
-	 * @var int
-	 */
-	const REMOTE_SQL_DB = 0x1004;
-	/**
-	 * @var int
-	 */
-	const REMOTE_SQL_DB_SCHEMA = 0x1008;
-	/**
-	 * @var int
-	 */
-	const REMOTE_WEB_SERVICE = 0x1020;
-	/**
-	 * @var int
-	 */
-	const LOCAL_RUNNER_SERVICE = 0x1040;
-	/**
-	 * @var string The system manager endpoint
-	 */
-	const SYSTEM_MANAGER_SERVICE = 'system';
-	/**
-	 * @var string The system manager endpoint
-	 */
-	const USER_MANAGER_SERVICE = 'user';
+    /**
+     * @var int
+     */
+    const SYSTEM_SERVICE = 0x0000;
+    /**
+     * @var int
+     */
+    const EMAIL_SERVICE = 0x0001;
+    /**
+     * @var int
+     */
+    const LOCAL_FILE_STORAGE = 0x0002;
+    /**
+     * @var int
+     */
+    const LOCAL_SQL_DB = 0x0004;
+    /**
+     * @var int
+     */
+    const LOCAL_SQL_DB_SCHEMA = 0x0008;
+    /**
+     * @var int
+     */
+    const NOSQL_DB = 0x0010;
+    /**
+     * @var int
+     */
+    const SALESFORCE = 0x0020;
+    /**
+     * @var int
+     */
+    const LOCAL_PORTAL_SERVICE = 0x0040;
+    /**
+     * @var int
+     */
+    const REMOTE_FILE_STORAGE = 0x1002;
+    /**
+     * @var int
+     */
+    const REMOTE_SQL_DB = 0x1004;
+    /**
+     * @var int
+     */
+    const REMOTE_SQL_DB_SCHEMA = 0x1008;
+    /**
+     * @var int
+     */
+    const REMOTE_WEB_SERVICE = 0x1020;
+    /**
+     * @var int
+     */
+    const LOCAL_RUNNER_SERVICE = 0x1040;
+    /**
+     * @var string The system manager endpoint
+     */
+    const SYSTEM_MANAGER_SERVICE = 'system';
+    /**
+     * @var string The system manager endpoint
+     */
+    const USER_MANAGER_SERVICE = 'user';
 
-	//*************************************************************************
-	//	Members
-	//*************************************************************************
+    //*************************************************************************
+    //	Members
+    //*************************************************************************
 
-	/**
-	 * @param int    $value        enumerated type value
-	 * @param string $service_name given name of the service, also returned as default
-	 *
-	 * @var array A map of classes for services
-	 */
-	protected static $_classMap = array(
-		self::SYSTEM_MANAGER_SERVICE => 'SystemManager',
-		self::USER_MANAGER_SERVICE   => 'UserManager',
-		self::LOCAL_PORTAL_SERVICE   => 'Portal',
-		self::LOCAL_FILE_STORAGE     => 'LocalFileSvc',
-		self::REMOTE_FILE_STORAGE    => 'RemoteFileSvc',
-		self::LOCAL_SQL_DB           => 'SqlDbSvc',
-		self::REMOTE_SQL_DB          => 'SqlDbSvc',
-		self::LOCAL_SQL_DB_SCHEMA    => 'SchemaSvc',
-		self::REMOTE_SQL_DB_SCHEMA   => 'SchemaSvc',
-		self::EMAIL_SERVICE          => 'EmailSvc',
-		self::NOSQL_DB               => 'NoSqlDbSvc',
-		self::SALESFORCE             => 'SalesforceDbSvc',
-		self::REMOTE_WEB_SERVICE     => 'RemoteWebSvc',
-	);
+    /**
+     * @param int    $value        enumerated type value
+     * @param string $service_name given name of the service, also returned as default
+     *
+     * @var array A map of classes for services
+     */
+    protected static $_classMap = array(
+        self::SYSTEM_MANAGER_SERVICE => 'SystemManager',
+        self::USER_MANAGER_SERVICE   => 'UserManager',
+        self::LOCAL_PORTAL_SERVICE   => 'Portal',
+        self::LOCAL_FILE_STORAGE     => 'LocalFileSvc',
+        self::REMOTE_FILE_STORAGE    => 'RemoteFileSvc',
+        self::LOCAL_SQL_DB           => 'SqlDbSvc',
+        self::REMOTE_SQL_DB          => 'SqlDbSvc',
+        self::LOCAL_SQL_DB_SCHEMA    => 'SchemaSvc',
+        self::REMOTE_SQL_DB_SCHEMA   => 'SchemaSvc',
+        self::EMAIL_SERVICE          => 'EmailSvc',
+        self::NOSQL_DB               => 'NoSqlDbSvc',
+        self::SALESFORCE             => 'SalesforceDbSvc',
+        self::REMOTE_WEB_SERVICE     => 'RemoteWebSvc',
+        self::LOCAL_RUNNER_SERVICE   => 'Runner',
+    );
 
-	//*************************************************************************
-	//	Methods
-	//*************************************************************************
+    //*************************************************************************
+    //	Methods
+    //*************************************************************************
 
-	/**
-	 * @param int    $value       enumerated type value
-	 * @param string $serviceName given name of the service, also returned as default
-	 *
-	 * @return string - associated file name of native service
-	 */
-	public static function getFileName( $value, $serviceName )
-	{
-		$_serviceName = $serviceName ? : null;
+    /**
+     * @param int    $value       enumerated type value
+     * @param string $serviceName given name of the service, also returned as default
+     *
+     * @return string - associated file name of native service
+     */
+    public static function getFileName( $value, $serviceName )
+    {
+        $_serviceName = $serviceName ? : null;
 
-		if ( null !== ( $_fileName = Option::get( static::$_classMap, $value ) ) )
-		{
-			return $_fileName;
-		}
+        if ( null !== ( $_fileName = Option::get( static::$_classMap, $value ) ) )
+        {
+            return $_fileName;
+        }
 
-		if ( static::SYSTEM_SERVICE == $value && !empty( $_serviceName ) )
-		{
-			if ( null !== ( $_fileName = Option::get( static::$_classMap, strtolower( $_serviceName ) ) ) )
-			{
-				return $_fileName;
-			}
+        if ( static::SYSTEM_SERVICE == $value && !empty( $_serviceName ) )
+        {
+            if ( null !== ( $_fileName = Option::get( static::$_classMap, strtolower( $_serviceName ) ) ) )
+            {
+                return $_fileName;
+            }
 
-			return static::$_classMap[static::SYSTEM_MANAGER_SERVICE];
-		}
+            return static::$_classMap[static::SYSTEM_MANAGER_SERVICE];
+        }
 
-		return $_serviceName;
-	}
+        return $_serviceName;
+    }
 }
