d551b3058cdca7a5787cd6b2f39b6f132b05ff20
Jerry Ablan <jerryablan@dreamfactory.com>
2014-04-07 16:25:09 -0400

    Removed CORS stuff from console app

--- src/Yii/Components/PlatformConsoleApplication.php
+++ src/Yii/Components/PlatformConsoleApplication.php
@@ -25,17 +25,13 @@ use DreamFactory\Platform\Events\DspEvent;
 use DreamFactory\Platform\Events\Enums\DspEvents;
 use DreamFactory\Platform\Events\EventDispatcher;
 use DreamFactory\Platform\Events\PlatformEvent;
-use DreamFactory\Platform\Exceptions\BadRequestException;
 use DreamFactory\Platform\Exceptions\InternalServerErrorException;
+use DreamFactory\Platform\Utility\CorsManager;
 use DreamFactory\Yii\Utility\Pii;
 use Kisma\Core\Enums\CoreSettings;
-use Kisma\Core\Enums\HttpMethod;
-use Kisma\Core\Enums\HttpResponse;
 use Kisma\Core\Interfaces\PublisherLike;
 use Kisma\Core\Interfaces\SubscriberLike;
 use Kisma\Core\Utility\Log;
-use Kisma\Core\Utility\Option;
-use Kisma\Core\Utility\Scalar;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -48,30 +44,6 @@ class PlatformConsoleApplication extends \CConsoleApplication implements Publish
     //	Constants
     //*************************************************************************
 
-    /**
-     * @var string The HTTP Option method
-     */
-    const CORS_OPTION_METHOD = 'OPTIONS';
-    /**
-     * @var string The allowed HTTP methods
-     */
-    const CORS_DEFAULT_ALLOWED_METHODS = 'GET, POST, PUT, DELETE, PATCH, MERGE, COPY, OPTIONS';
-    /**
-     * @var string The allowed HTTP headers
-     */
-    const CORS_DEFAULT_ALLOWED_HEADERS = 'Content-Type, X-Requested-With, X-DreamFactory-Application-Name, X-Application-Name, X-DreamFactory-Session-Token';
-    /**
-     * @var int The default number of seconds to allow this to be cached. Default is 15 minutes.
-     */
-    const CORS_DEFAULT_MAX_AGE = 900;
-    /**
-     * @var string The private CORS configuration file
-     */
-    const CORS_DEFAULT_CONFIG_FILE = '/cors.config.json';
-    /**
-     * @var string The session key for CORS configs
-     */
-    const CORS_WHITELIST_KEY = 'cors.config';
     /**
      * @var string The default DSP resource namespace
      */
@@ -118,19 +90,6 @@ class PlatformConsoleApplication extends \CConsoleApplication implements Publish
      */
     protected static $_namespaceMap = array( self::NS_MODELS => array(), self::NS_SERVICES => array(), self::NS_RESOURCES => array() );
     /**
-     * @var array An indexed array of white-listed hosts (ajax.example.com or foo.bar.com or just bar.com)
-     */
-    protected $_corsWhitelist = array();
-    /**
-     * @var bool    If true, the CORS headers will be sent automatically before dispatching the action.
-     *              NOTE: "OPTIONS" calls will always get headers, regardless of the setting. All other requests respect the setting.
-     */
-    protected $_autoAddHeaders = true;
-    /**
-     * @var bool If true, adds some extended information about the request in the form of X-DreamFactory-* headers
-     */
-    protected $_extendedHeaders = true;
-    /**
      * @var array The namespaces that contain resources. Used by the routing engine
      */
     protected $_resourceNamespaces = array();
@@ -157,8 +116,6 @@ class PlatformConsoleApplication extends \CConsoleApplication implements Publish
     protected function init()
     {
         parent::init();
-
-        $this->_loadCorsConfig();
 
         //	Debug options
         static::$_enableProfiler = Pii::getParam( 'dsp.enable_profiler', false );
@@ -273,49 +230,7 @@ class PlatformConsoleApplication extends \CConsoleApplication implements Publish
     {
         //	Start the request-only profile
         $this->startProfiler( 'app.request' );
-
         $this->_requestObject = Request::createFromGlobals();
-
-        //  Call getter to get CORS headers auto-added
-        $_response = $this->_useResponseObject ? $this->getResponseObject() : null;
-
-        switch ( $this->_requestObject->getMethod() )
-        {
-            case HttpMethod::TRACE:
-                Log::error(
-                    'HTTP TRACE received!',
-                    array(
-                        'server'  => $_SERVER,
-                        'request' => $_REQUEST,
-                    )
-                );
-
-                throw new BadRequestException();
-
-            case HttpMethod::OPTIONS:
-                if ( $this->_useResponseObject )
-                {
-                    $_response->setStatusCode( HttpResponse::NoContent )->setContent( '' )->headers->set( 'Content-Type', 'text/plain', true );
-                    $_response->send();
-                }
-                else
-                {
-                    header( 'HTTP/1.1 204' );
-                    header( 'content-length: 0' );
-                    header( 'content-type: text/plain' );
-
-                    $this->_useResponseObject = false;
-                    $this->addCorsHeaders();
-                }
-
-                return Pii::end();
-        }
-
-        //	Auto-add the CORS headers...
-        if ( $this->_autoAddHeaders )
-        {
-            $this->addCorsHeaders();
-        }
 
         //	Load any plug-ins
         $this->_loadPlugins();
@@ -375,432 +290,12 @@ class PlatformConsoleApplication extends \CConsoleApplication implements Publish
     {
         static::getDispatcher()->removeListener( $eventName, $listener );
     }
-    //*************************************************************************
-    //	CORS Support
-    //*************************************************************************
-
-    /**
-     * @param array|bool $whitelist     Set to "false" to reset the internal method cache.
-     * @param bool       $returnHeaders If true, the headers are return in an array and NOT sent
-     * @param bool       $sendHeaders   If false, the headers will NOT be sent. Defaults to true. $returnHeaders takes precedence
-     *
-     * @throws RestException
-     * @throws \InvalidArgumentException
-     * @return bool|array
-     */
-    public function addCorsHeaders( $whitelist = array(), $returnHeaders = false, $sendHeaders = true )
-    {
-        $_headers = $this->_buildCorsHeaders( $whitelist );
-
-        if ( $returnHeaders )
-        {
-            return $_headers;
-        }
-
-        if ( $this->_useResponseObject )
-        {
-            //  Initialize the response object if not already
-            $this->getResponseObject();
-
-            foreach ( $_headers as $_key => $_value )
-            {
-                $this->_responseObject->headers->set( $_key, $_value, true );
-            }
-
-            if ( $sendHeaders )
-            {
-                $this->_responseObject->sendHeaders();
-            }
-        }
-        elseif ( $sendHeaders )
-        {
-            //	Dump the headers
-            foreach ( $_headers as $_key => $_value )
-            {
-                header( $_key . ': ' . $_value, true );
-            }
-        }
-
-        return true;
-    }
-
-    /**
-     * Builds a cache key for a request and returns the constituent parts
-     *
-     * @throws \InvalidArgumentException
-     * @return array
-     */
-    protected function _buildCacheKey()
-    {
-        $_origin = trim( Option::server( 'HTTP_ORIGIN' ) );
-        $_requestSource = Option::server( 'SERVER_NAME', \Kisma::get( 'platform.host_name', gethostname() ) );
-
-        if ( false === ( $_originParts = $this->_parseUri( $_origin ) ) )
-        {
-            //	Not parse-able, set to itself, check later (testing from local files - no session)?
-            Log::warning( 'Unable to parse received origin: [' . $_origin . ']' );
-            $_originParts = $_origin;
-        }
-
-        $_originUri = $this->_normalizeUri( $_originParts );
-        $_key = sha1( $_requestSource . $_originUri );
-
-        return array(
-            $_key,
-            $_origin,
-            $_requestSource,
-            $_originParts,
-            $_originUri,
-        );
-    }
-
-    /**
-     * @param array|bool $whitelist Set to "false" to reset the internal method cache.
-     *
-     * @throws \InvalidArgumentException
-     * @throws RestException
-     * @return array
-     */
-    protected function _buildCorsHeaders( $whitelist = array() )
-    {
-        static $_cache = array();
-        static $_cacheVerbs = array();
-
-        //	Reset the cache before processing...
-        if ( false === $whitelist )
-        {
-            $_cache = array();
-            $_cacheVerbs = array();
-
-            return true;
-        }
-
-        $_originUri = null;
-        $_headers = array();
-
-        //  Deal with CORS headers
-        list(
-            $_key,
-            $_origin,
-            $_requestSource,
-            $_originParts,
-            $_originUri,
-            ) = $this->_buildCacheKey();
-
-        //	Was an origin header passed? If not, don't do CORS.
-        if ( !empty( $_origin ) )
-        {
-            //	Not in cache, check it out...
-            if ( !in_array( $_key, $_cache ) )
-            {
-                if ( false === ( $_allowedMethods = $this->_allowedOrigin( $_originParts, $_requestSource ) ) )
-                {
-                    Log::error( 'Unauthorized origin rejected via CORS > Source: ' . $_requestSource . ' > Origin: ' . $_originUri );
-
-                    /**
-                     * No sir, I didn't like it.
-                     *
-                     * @link http://www.youtube.com/watch?v=VRaoHi_xcWk
-                     */
-                    throw new RestException( HttpResponse::Forbidden );
-                }
-            }
-            else
-            {
-                $_originUri = $_cache[$_key];
-                $_allowedMethods = Option::getDeep( $_cacheVerbs, $_key, 'allowed_methods' );
-                $_headers = Option::getDeep( $_cacheVerbs, $_key, 'headers' );
-            }
-
-            if ( !empty( $_originUri ) )
-            {
-                $_headers['Access-Control-Allow-Origin'] = $_originUri;
-            }
-
-            $_headers['Access-Control-Allow-Credentials'] = 'true';
-            $_headers['Access-Control-Allow-Headers'] = static::CORS_DEFAULT_ALLOWED_HEADERS;
-            $_headers['Access-Control-Allow-Methods'] = $_allowedMethods;
-            $_headers['Access-Control-Max-Age'] = static::CORS_DEFAULT_MAX_AGE;
-
-            //	Store in cache...
-            $_cache[$_key] = $_originUri;
-            $_cacheVerbs[$_key] = array(
-                'allowed_methods' => $_allowedMethods,
-                'headers'         => $_headers
-            );
-        }
-
-        return $_headers + $this->_buildExtendedHeaders( $_requestSource, $_originUri, !empty( $_origin ) );
-    }
-
-    /**
-     * @param string $requestSource
-     * @param string $originUri
-     * @param bool   $whitelisted If the origin is whitelisted
-     *
-     * @return array
-     */
-    protected function _buildExtendedHeaders( $requestSource, $originUri, $whitelisted = true )
-    {
-        $_headers = array();
-
-        if ( $this->_extendedHeaders )
-        {
-            $_headers['X-DreamFactory-Source'] = $requestSource;
-
-            if ( $whitelisted )
-            {
-                $_headers['X-DreamFactory-Origin-Whitelisted'] = preg_match( '/^([\w_-]+\.)*' . $requestSource . '$/', $originUri );
-            }
-        }
-
-        return $_headers;
-    }
-
-    /**
-     * @param string|array $origin     The parse_url value of origin
-     * @param array        $additional Additional origins to allow
-     *
-     * @throws \InvalidArgumentException
-     * @return bool|array false if not allowed, otherwise array of verbs allowed
-     */
-    protected function _allowedOrigin( $origin, $additional = array() )
-    {
-        foreach ( array_merge( $this->_corsWhitelist, Option::clean( $additional ) ) as $_hostInfo )
-        {
-            $_allowedMethods = static::CORS_DEFAULT_ALLOWED_METHODS;
-
-            if ( is_array( $_hostInfo ) )
-            {
-                //	If is_enabled prop not there, assuming enabled.
-                if ( !Scalar::boolval( Option::get( $_hostInfo, 'is_enabled', true ) ) )
-                {
-                    continue;
-                }
-
-                if ( null === ( $_whiteGuy = Option::get( $_hostInfo, 'host' ) ) )
-                {
-                    Log::error( 'CORS whitelist info does not contain a "host" parameter!' );
-                    continue;
-                }
-
-                if ( isset( $_hostInfo['verbs'] ) )
-                {
-                    if ( false === array_search( static::CORS_OPTION_METHOD, $_hostInfo['verbs'] ) )
-                    {
-                        // add OPTION to allowed list
-                        $_hostInfo['verbs'][] = static::CORS_OPTION_METHOD;
-                    }
-                    $_allowedMethods = implode( ', ', $_hostInfo['verbs'] );
-                }
-            }
-            else
-            {
-                $_whiteGuy = $_hostInfo;
-            }
-
-            //	All allowed?
-            if ( '*' == $_whiteGuy )
-            {
-                return $_allowedMethods;
-            }
-
-            if ( false === ( $_whiteParts = $this->_parseUri( $_whiteGuy ) ) )
-            {
-                continue;
-            }
-
-            //	Check for un-parsed origin, 'null' sent when testing js files locally
-            if ( is_array( $origin ) )
-            {
-                //	Is this origin on the whitelist?
-                if ( $this->_compareUris( $origin, $_whiteParts ) )
-                {
-                    return $_allowedMethods;
-                }
-            }
-        }
-
-        return false;
-    }
-
-    /**
-     * @param array $first
-     * @param array $second
-     *
-     * @return bool
-     */
-    protected function _compareUris( $first, $second )
-    {
-        return ( $first['scheme'] == $second['scheme'] ) && ( $first['host'] == $second['host'] ) && ( $first['port'] == $second['port'] );
-    }
-
-    /**
-     * @param string $uri
-     * @param bool   $normalize
-     *
-     * @throws \InvalidArgumentException
-     * @return array
-     */
-    protected function _parseUri( $uri, $normalize = false )
-    {
-        if ( false === ( $_parts = parse_url( $uri ) ) || !( isset( $_parts['host'] ) || isset( $_parts['path'] ) ) )
-        {
-            return false;
-        }
-
-        if ( isset( $_parts['path'] ) && !isset( $_parts['host'] ) )
-        {
-            //	Special case, handle this generically later
-            if ( 'null' == $_parts['path'] )
-            {
-                return 'null';
-            }
-
-            $_parts['host'] = $_parts['path'];
-            unset( $_parts['path'] );
-        }
-
-        $_protocol = $this->_requestObject->isSecure() ? 'https' : 'http';
-
-        $_uri = array(
-            'scheme' => Option::get( $_parts, 'scheme', $_protocol ),
-            'host'   => Option::get( $_parts, 'host' ),
-            'port'   => Option::get( $_parts, 'port' ),
-        );
-
-        return $normalize ? $this->_normalizeUri( $_uri ) : $_uri;
-    }
-
-    /**
-     * @param array $parts Return from \parse_url
-     *
-     * @return string
-     */
-    protected function _normalizeUri( $parts )
-    {
-        return is_array( $parts ) ?
-            ( isset( $parts['scheme'] ) ? $parts['scheme'] : 'http' ) . '://' . $parts['host'] . ( isset( $parts['port'] ) ? ':' . $parts['port'] : null )
-            : $parts;
-    }
-
-    /**
-     * Loads the CORS whitelist from the session. If not there, it's loaded and stuffed in there.
-     *
-     * @throws \DreamFactory\Platform\Exceptions\InternalServerErrorException
-     * @throws RestException
-     * @return $this
-     */
-    protected function _loadCorsConfig()
-    {
-        static $_whitelist = null;
-
-        if ( null === $_whitelist && null === ( $_whitelist = \Kisma::get( 'cors.whitelist' ) ) )
-        {
-            //	Get CORS data from config file
-            $_config = Platform::getStorageBasePath( static::CORS_DEFAULT_CONFIG_FILE, true, true );
-
-            if ( !file_exists( $_config ) )
-            {
-                //  In old location?
-                $_config = Platform::getPrivatePath( static::CORS_DEFAULT_CONFIG_FILE, true, true );
-            }
-
-            $_whitelist = array();
-
-            if ( file_exists( $_config ) )
-            {
-                if ( false !== ( $_content = @file_get_contents( $_config ) ) && !empty( $_content ) )
-                {
-                    $_whitelist = json_decode( $_content, true );
-
-                    if ( JSON_ERROR_NONE != json_last_error() )
-                    {
-                        throw new InternalServerErrorException( 'The CORS configuration file is corrupt. Cannot continue.' );
-                    }
-                }
-            }
-
-            \Kisma::set( 'cors.whitelist', $_whitelist );
-        }
-
-        return $this->setCorsWhitelist( $_whitelist );
-    }
-
-    //*************************************************************************
-    //	Accessors
-    //*************************************************************************
-
-    /**
-     * @param array $corsWhitelist
-     *
-     * @throws RestException
-     * @return PlatformWebApplication
-     */
-    public function setCorsWhitelist( $corsWhitelist )
-    {
-        $this->_corsWhitelist = $corsWhitelist;
-
-        //	Reset the header cache
-        $this->_buildCorsHeaders( false );
-
-        return $this;
-    }
-
-    /**
-     * @return array
-     */
-    public function getCorsWhitelist()
-    {
-        return $this->_corsWhitelist;
-    }
-
-    /**
-     * @param boolean $autoAddHeaders
-     *
-     * @return PlatformWebApplication
-     */
-    public function setAutoAddHeaders( $autoAddHeaders = true )
-    {
-        $this->_autoAddHeaders = $autoAddHeaders;
-
-        return $this;
-    }
-
-    /**
-     * @return boolean
-     */
-    public function getAutoAddHeaders()
-    {
-        return $this->_autoAddHeaders;
-    }
-
-    /**
-     * @param boolean $extendedHeaders
-     *
-     * @return PlatformWebApplication
-     */
-    public function setExtendedHeaders( $extendedHeaders = true )
-    {
-        $this->_extendedHeaders = $extendedHeaders;
-
-        return $this;
-    }
-
-    /**
-     * @return boolean
-     */
-    public function getExtendedHeaders()
-    {
-        return $this->_extendedHeaders;
-    }
 
     /**
      * @param bool $createIfNull If true, the default, the response object will be created if it hasn't already
      * @param bool $sendHeaders
      *
-     * @throws \InvalidArgumentException
-     * @throws RestException
+     * @throws \DreamFactory\Platform\Utility\RestException
      * @return \Symfony\Component\HttpFoundation\Response
      */
     public function getResponseObject( $createIfNull = true, $sendHeaders = true )
@@ -808,11 +303,7 @@ class PlatformConsoleApplication extends \CConsoleApplication implements Publish
         if ( null === $this->_responseObject && $createIfNull )
         {
             $this->_responseObject = Response::create();
-
-            if ( $this->_autoAddHeaders )
-            {
-                $this->addCorsHeaders( array(), false, $sendHeaders );
-            }
+            CorsManager::autoSendHeaders();
         }
 
         return $this->_responseObject;
