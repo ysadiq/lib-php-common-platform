8e900d6e9b5f4bd3d6f7940377f171057021e845
Jerry Ablan <jerryablan@dreamfactory.com>
2014-08-04 20:50:34 -0400

    Added Environment resource for PaaS testing

---
+++ src/Resources/System/Environment.swagger.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * This file is part of the DreamFactory Services Platform(tm) SDK For PHP
+ *
+ * DreamFactory Services Platform(tm) <http://github.com/dreamfactorysoftware/dsp-core>
+ * Copyright 2012-2014 DreamFactory Software, Inc. <developer-support@dreamfactory.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+return array(
+
+    //-------------------------------------------------------------------------
+    //	APIs
+    //-------------------------------------------------------------------------
+
+    'apis'   => array(
+        array(
+            'path'        => '/{api_name}/environment',
+            'operations'  => array(
+                array(
+                    'method'     => 'GET',
+                    'summary'    => 'getEnvironment() - Retrieve environment information.',
+                    'nickname'   => 'getEnvironment',
+                    'type'       => 'EnvironmentResponse',
+                    'event_name' => '{api_name}.environment.read',
+                    'notes'      => 'The retrieved information describes the container/machine on which the DSP resides.',
+                ),
+            ),
+            'description' => 'Operations for system configuration options.',
+        ),
+    ),
+    //-------------------------------------------------------------------------
+    //	Models
+    //-------------------------------------------------------------------------
+
+    'models' => array(
+        'EnvironmentResponse' => array(
+            'id'         => 'EnvironmentResponse',
+            'properties' => array(),
+        ),
+    )
+);
