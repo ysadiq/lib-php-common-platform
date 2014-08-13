5cad9df745f8db169f3c794034471b8498c9a850
Jerry Ablan <jerryablan@dreamfactory.com>
2014-08-13 18:43:20 -0400

    corrected wrong namespace in views

--- src/Views/System/Service.php
+++ src/Views/System/Service.php
@@ -17,7 +17,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-namespace DreamFactory\Platform\Resources\System;
+namespace DreamFactory\Platform\Views\System;
 
 use DreamFactory\Platform\Resources\BaseSystemRestResource;
 use DreamFactory\Platform\Services\SwaggerManager;
