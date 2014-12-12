07a7295a3dfc7202e6931bc3bc8c1a8909aa7ba4
Lee Hicks <leehicks@dreamfactory.com>
2014-12-12 14:27:25 -0500

    allow empty string on base rest exception

--- src/Exceptions/RestException.php
+++ src/Exceptions/RestException.php
@@ -32,94 +32,98 @@ use Kisma\Core\Utility\Option;
  */
 class RestException extends PlatformServiceException implements HttpResponse
 {
-	//*************************************************************************
-	//* Members
-	//*************************************************************************
+    //*************************************************************************
+    //* Members
+    //*************************************************************************
 
-	/**
-	 * @var int HTTP status code, such as 403, 404, 500, etc.
-	 */
-	protected $_statusCode;
+    /**
+     * @var int HTTP status code, such as 403, 404, 500, etc.
+     */
+    protected $_statusCode;
 
-	//*************************************************************************
-	//* Methods
-	//*************************************************************************
+    //*************************************************************************
+    //* Methods
+    //*************************************************************************
 
-	/**
-	 * Constructor.
-	 *
-	 * @param int    $status  HTTP status code, such as 404, 500, etc.
-	 * @param string $message error message
-	 * @param int    $code    error code
-	 * @param mixed  $previous
-	 * @param mixed  $context Additional information for downstream consumers
-	 */
-	public function __construct( $status, $message = null, $code = null, $previous = null, $context = null )
-	{
-		$this->_statusCode = $status;
-		$code = $code ? : $this->_statusCode;
+    /**
+     * Constructor.
+     *
+     * @param int    $status  HTTP status code, such as 404, 500, etc.
+     * @param string $message error message
+     * @param int    $code    error code
+     * @param mixed  $previous
+     * @param mixed  $context Additional information for downstream consumers
+     */
+    public function __construct( $status, $message = null, $code = null, $previous = null, $context = null )
+    {
+        $this->_statusCode = $status;
+        $code = $code ?: $this->_statusCode;
 
-		if ( empty( $message ) )
-		{
-			$_name = \Kisma\Core\Enums\HttpResponse::nameof( $code );
-			$message = Inflector::camelize( Inflector::neutralize( $_name ), '_', true );
-		}
+        if ( is_null( $message ) )
+        {
+            $_name = \Kisma\Core\Enums\HttpResponse::nameof( $code );
+            $message = Inflector::camelize( Inflector::neutralize( $_name ), '_', true );
+        }
+        elseif ( !is_string( $message ) )
+        {
+            $message = strval( $message );
+        }
 
-		parent::__construct( $message, $code, $previous, $context );
+        parent::__construct( $message, $code, $previous, $context );
 
-		Log::error(
-			'REST Exception #' . $code . ' > ' . $message,
-			array(
-				'host'        => Option::server( 'HTTP_HOST', \gethostname() ),
-				'request_uri' => Option::server( 'REQUEST_URI' ),
-				'source_ip'   => Option::server( 'REMOTE_ADDR' ),
-				'sapi_name'   => \php_sapi_name(),
-			)
-		);
-	}
+        Log::error(
+            'REST Exception #' . $code . ' > ' . $message,
+            array(
+                'host'        => Option::server( 'HTTP_HOST', \gethostname() ),
+                'request_uri' => Option::server( 'REQUEST_URI' ),
+                'source_ip'   => Option::server( 'REMOTE_ADDR' ),
+                'sapi_name'   => \php_sapi_name(),
+            )
+        );
+    }
 
-	/**
-	 * @param int $statusCode
-	 *
-	 * @return RestException
-	 */
-	public function setStatusCode( $statusCode )
-	{
-		$this->_statusCode = $statusCode;
+    /**
+     * @param int $statusCode
+     *
+     * @return RestException
+     */
+    public function setStatusCode( $statusCode )
+    {
+        $this->_statusCode = $statusCode;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * @return int
-	 */
-	public function getStatusCode()
-	{
-		return $this->_statusCode;
-	}
+    /**
+     * @return int
+     */
+    public function getStatusCode()
+    {
+        return $this->_statusCode;
+    }
 
-	/**
-	 * @return int
-	 */
-	public function statusCode()
-	{
-		return $this->_statusCode;
-	}
+    /**
+     * @return int
+     */
+    public function statusCode()
+    {
+        return $this->_statusCode;
+    }
 
-	/**
-	 * This ONLY exists because Yii uses public variables ARGH!
-	 *
-	 * @param string $name
-	 *
-	 * @return mixed
-	 */
-	public function __get( $name )
-	{
-		$name = strtolower( $name );
+    /**
+     * This ONLY exists because Yii uses public variables ARGH!
+     *
+     * @param string $name
+     *
+     * @return mixed
+     */
+    public function __get( $name )
+    {
+        $name = strtolower( $name );
 
-		if ( method_exists( $this, 'get' . $name ) )
-		{
-			return $this->{'get' . $name}();
-		}
-	}
+        if ( method_exists( $this, 'get' . $name ) )
+        {
+            return $this->{'get' . $name}();
+        }
+    }
 }
